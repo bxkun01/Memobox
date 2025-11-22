097a4794fe1bc74d479aa0d7b22848ab4d64e87e
bxkun01 <levianthan404@gmail.com>
2025-11-22 22:05:12 +0545

    added somethings

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/aedan/memobox/.venv
+set -gx VIRTUAL_ENV /home/yuefeng__/Memobox/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
