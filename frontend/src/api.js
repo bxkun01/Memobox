b0ecaa72181a8655abf419a191c214ddff31e6fa
Bxkun <levianthan404@gmail.com>
2025-12-20 15:47:56 +0545

    Update API base URL to new endpoint

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { ACCESS_TOKEN } from './constants'
 
 const api = axios.create({
-    baseURL: "https://7a8f4238-7240-410e-9d8e-eff9cd8cfddb.e1-us-east-azure.choreoapps.dev/"
+    baseURL: "https://memobox-rcp8.onrender.com/"
 })
 
 api.interceptors.request.use(
